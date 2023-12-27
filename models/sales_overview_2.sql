8923b9867f0c511aa33a93d29c4d8cd749a3defc
Omni Solutions <se-team@exploreomni.com>
2023-12-27 23:04:50 +0000

    moving files around

--- models/sales_overview_2.sql
+++
@@ -1,7 +0,0 @@
-SELECT DATE_TRUNC('MONTH', "CREATED_AT") AS "order_items.created_at[month]__raw",
-    COALESCE(SUM("SALE_PRICE"), 0) AS "order_items.total_sale_price",
-    TO_CHAR(DATE_TRUNC('MONTH', "CREATED_AT"), 'YYYY-MM') AS "order_items.created_at[month]"
-FROM "ORDER_ITEMS" AS "order_items"
-GROUP BY DATE_TRUNC('MONTH', "CREATED_AT")
-ORDER BY DATE_TRUNC('MONTH', "CREATED_AT") NULLS FIRST
-LIMIT 1000
