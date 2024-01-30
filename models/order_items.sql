604fc37b064dd585a4eb9d57896fd6022facc45e
Omni Solutions <se-team@exploreomni.com>
2024-01-30 19:04:27 +0000

    removing id+1

--- models/order_items.sql
+++ models/order_items.sql
@@ -1,7 +1,6 @@
 {{ config(materialized='table') }}
 
 SELECT "order_items"."ID"  AS "id",
-    "order_items"."ID"+1  AS "id_plus_one",
     "order_items"."ORDER_ID" AS "order_id",
     "order_items"."USER_ID" AS "user_id",
     "order_items"."INVENTORY_ITEM_ID" AS "inventory_item_id",
