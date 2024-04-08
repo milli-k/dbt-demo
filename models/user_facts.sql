1b18be08a67bf923c81f76f8ab411e2af9fe5ff4
Omni Solutions <se-team@exploreomni.com>
2024-04-08 22:21:24 +0000

    testing prod workflow

--- models/user_facts.sql
+++ models/user_facts.sql
@@ -4,10 +4,7 @@ SELECT "users"."ID" AS "users.id",
     MIN("order_items"."CREATED_AT") AS "order_items.created_at_min_1__raw",
     COALESCE(SUM("order_items"."SALE_PRICE"), 0) AS "order_items.total_sale_price",
     COUNT("order_items"."ID") AS "order_items.count",
-    TO_CHAR(DATE_TRUNC('DAY', "users"."CREATED_AT"), 'YYYY-MM-DD') AS "users.created_at[date]",
-    TO_CHAR(MIN("order_items"."CREATED_AT"), 'YYYY-MM-DD HH24:MI:SS.FF3') AS "order_items.created_at_min_1"
-FROM "USERS" AS "users"
-    LEFT JOIN "ORDER_ITEMS" AS "order_items" ON "users"."ID" = "order_items"."USER_ID"
+FROM PUBLIC."USERS" AS "users"
+    LEFT JOIN PUBLIC."ORDER_ITEMS" AS "order_items" ON "users"."ID" = "order_items"."USER_ID"
 GROUP BY 1, 2, 3
 ORDER BY 3 NULLS FIRST
-LIMIT 1000
