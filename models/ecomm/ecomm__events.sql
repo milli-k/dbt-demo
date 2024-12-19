f5ab1e8fdce80164b35b860288605337175c7e1e
Jade K <jade@omni.co>
2024-12-19 23:51:40 +0000

    remove ecomm views because role has insufficient permissions

--- models/ecomm/ecomm__events.sql
+++
@@ -1,5 +0,0 @@
-{{ config(alias='events') }}
-
-select
-  {{ dbt_utils.star(source('ecomm', 'events')) }}
-from {{ source('ecomm', 'events') }}
--- models/ecomm/ecomm__products.sql
+++
@@ -1,5 +0,0 @@
-{{ config(alias='products') }}
-
-select
-  {{ dbt_utils.star(source('ecomm', 'products')) }}
-from {{ source('ecomm', 'products') }}
