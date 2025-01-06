eb0fe002c6558982c6c02877a79c39691feb762a
Omni Solutions <se-team@exploreomni.com>
2025-01-06 22:21:24 +0000

    update generate_model_yaml

--- analyses/generate_model_yaml.sql
+++ analyses/generate_model_yaml.sql
@@ -1,5 +1,3 @@
-
-{% set models_to_generate = codegen.get_models(directory='intermediate/ecomm') %}
 {{ codegen.generate_model_yaml(
-    model_names = models_to_generate
+    model_names = ['mrt_ecomm__sessions']
 ) }}
