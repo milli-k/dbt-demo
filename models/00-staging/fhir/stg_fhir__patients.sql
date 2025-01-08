7fd8b53925fc297d30a893d45d66090915a28a2f
Jade K <jade@omni.co>
2025-01-08 23:58:54 +0000

    add organizations, make patients more robust

--- models/00-staging/fhir/stg_fhir__patients.sql
+++ models/00-staging/fhir/stg_fhir__patients.sql
@@ -1,26 +1,48 @@
-with raw as (
-    select variant_col:entry[0] as json_column
-    from {{ source('fhir', 'raw_fhir') }}
-)
+with
+    raw as (
+        select variant_col:"entry"[0]."resource"::variant as resource_raw
+        from {{ source("fhir", "raw_fhir") }}
+        where
+            lower(variant_col:"entry"[0]."resource"."resourceType"::varchar) = 'patient'
+    )
 select
-    
-    json_column:"resource"."id"::varchar as id,
-    json_column:"resource"."birthDate"::date as birth_date,
-    json_column:"resource"."gender"::varchar as gender,
-    json_column:"resource"."maritalStatus".text::varchar as marital_status,
-    json_column:"resource"."multipleBirthBoolean"::boolean as multiple_birth,
-    json_column:"resource"."address"[0].city::varchar as city,
-    json_column:"resource"."address"[0].country::varchar as country,
-    json_column:"resource"."address"[0].postalCode::varchar as postal_code,
-    json_column:"resource"."address"[0].state::varchar as state,
-    json_column:"resource"."address"[0].line[0]::varchar as address_line_1,
-    json_column:"resource"."address"[0].line[1]::varchar as address_line_2,
-    json_column:"resource"."telecom"[0].value::varchar as phone_number,
-    json_column:"resource"."name"::variant as name,
-    json_column:"resource"."communication"[0]:"language":coding[0] as communication,
-    json_column:"resource"."extension"::variant as extension,
-    json_column:"resource"."identifier"::variant as identifier,
+    resource_raw:"id"::varchar as id,
+    resource_raw:"name"[0]."family"::varchar as family_name,
+    array_to_string(resource_raw:"name"[0]."given"::array, ' ') as given_names,
+    resource_raw:"telecom"[0]."value"::varchar as phone_number,
+    resource_raw:"birthDate"::date as birth_date,
+    resource_raw:"deceasedDateTime"::timestamp as deceased_date_time,
+    resource_raw:"gender"::varchar as gender,
+    resource_raw:"maritalStatus".text::varchar as marital_status,
+    resource_raw:"multipleBirthBoolean"::boolean as multiple_birth,
+    resource_raw:"address"[0]."city"::varchar as city,
+    resource_raw:"address"[0]."country"::varchar as country,
+    resource_raw:"address"[0]."postalCode"::varchar as postal_code,
+    resource_raw:"address"[0]."state"::varchar as state,
+    resource_raw:"address"[0]."line"[0]::varchar as address_line_1,
+    resource_raw:"address"[0]."line"[1]::varchar as address_line_2,
+    resource_raw:"address"[0]."extension"[0]."extension"[0]."valueDecimal"::varchar
+    as latitude,
+    resource_raw:"address"[0]."extension"[0]."extension"[0]."valueDecimal"::varchar
+    as longitude,
+    resource_raw:"telecom"[0]."value"::varchar as phone_number,
+    resource_raw:"extension"[0]."valueCodeableConcept"."coding"[0]."display"::varchar
+    as race,
+    resource_raw:"extension"[1]."valueCodeableConcept"."coding"[0]."display"::varchar
+    as ethnicity,
+    resource_raw:"extension"[2]."valueAddress"::object as birth_place,
+    resource_raw:"extension"[3]."valueString"::varchar as mothers_maiden_name,
+    resource_raw:"extension"[4]."valueCode"::varchar as birth_sex,
+    try_to_boolean(
+        resource_raw:"extension"[5]."valueBoolean"::varchar
+    ) as intepreter_required,
+    resource_raw:"extension"[8]."valueString"::varchar as ssn,
+    resource_raw:"name"::variant as name,
+    resource_raw:"communication"[0]:"language":coding[0] as communication,
+    resource_raw:"extension"::variant as extension,
+    resource_raw:"identifier"::variant as identifier,
     -- update later
-    json_column:"resource".text::variant as text_info,
-    json_column
+    resource_raw:text::variant as text_info,
+    resource_raw
 from raw
+group by all
