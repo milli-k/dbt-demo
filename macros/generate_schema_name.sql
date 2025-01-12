f3e127b61f0e89ec0f4d3eb731fe091a292ad5cd
Jade K <jade@omni.co>
2025-01-12 02:35:50 +0000

    fix generate schema back to target name, add auto select grants to project, and update observation schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,10 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- set environment_type = env_var('DBT_ENVIRONMENT', 'dev') -%}
 
-    {%- if environment_type == 'dev' and custom_schema_name is not none -%}
+    {%- if target.name == 'dev' and custom_schema_name is not none -%}
         {{ default_schema }}_{{ custom_schema_name | trim }}
-    {%- elif environment_type == 'prod' and custom_schema_name is not none -%}
+    {%- elif target.name == 'prod' and custom_schema_name is not none -%}
         {{ custom_schema_name | trim }}
     {%- else -%}
         {{ default_schema }}
