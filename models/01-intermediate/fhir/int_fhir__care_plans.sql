782db402786d32a233763fc355f165097bd8b97f
Jade K <jade@omni.co>
2025-01-16 16:44:13 +0000

    care_plans

--- models/01-intermediate/fhir/int_fhir__care_plans.sql
+++ models/01-intermediate/fhir/int_fhir__care_plans.sql
@@ -1,3 +1,5 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__care_plans')) }}
+  timestampadd(year, extract(year, current_date()) - 2017, start_ts) as start_ts,
+  timestampadd(year, extract(year, current_date()) - 2017, end_ts) as end_ts,
+  {{ dbt_utils.star(ref('stg_fhir__care_plans'), except=["start_ts", "end_ts"]) }}
 from {{ ref('stg_fhir__care_plans') }}
--- models/02-mart/fhir/care_plans.sql
+++ models/02-mart/fhir/care_plans.sql
@@ -1,3 +1,3 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__care_plans')) }}
-from {{ ref('stg_fhir__care_plans') }}
+  {{ dbt_utils.star(ref('int_fhir__care_plans')) }}
+from {{ ref('int_fhir__care_plans') }}
