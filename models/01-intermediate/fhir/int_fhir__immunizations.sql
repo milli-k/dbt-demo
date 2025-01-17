018d27f16ce29607e356d06f76cef99d9355be79
Jade K <jade@omni.co>
2025-01-17 22:00:42 +0000

    immunizations

--- models/01-intermediate/fhir/int_fhir__immunizations.sql
+++ models/01-intermediate/fhir/int_fhir__immunizations.sql
@@ -1,3 +1,4 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__immunizations')) }}
-from {{ ref('stg_fhir__immunizations') }}
+    timestampadd(year, extract(year, current_date()) - 2017, date_ts) as date_ts,
+    {{ dbt_utils.star(ref("stg_fhir__immunizations"), except=["date_ts"]) }}
+from {{ ref("stg_fhir__immunizations") }}
--- models/02-mart/fhir/immunizations.sql
+++ models/02-mart/fhir/immunizations.sql
@@ -1,3 +1,3 @@
 select
   {{ dbt_utils.star(ref('stg_fhir__immunizations')) }}
-from {{ ref('stg_fhir__immunizations') }}
+from {{ ref('int_fhir__immunizations') }}
