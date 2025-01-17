de378c21fb76d35197bf8d111575b04fd38fca97
Jade K <jade@omni.co>
2025-01-17 21:47:31 +0000

    encounters

--- models/01-intermediate/fhir/int_fhir__encounters.sql
+++ models/01-intermediate/fhir/int_fhir__encounters.sql
@@ -1,3 +1,5 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__encounters')) }}
-from {{ ref('stg_fhir__encounters') }}
+    timestampadd(year, extract(year, current_date()) - 2017, start_ts) as start_ts,
+    timestampadd(year, extract(year, current_date()) - 2017, end_ts) as end_ts,
+    {{ dbt_utils.star(ref("stg_fhir__encounters"), except=["start_ts", "end_ts"]) }}
+from {{ ref("stg_fhir__encounters") }}
