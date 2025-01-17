e5762237d51412b091caec58872684b92468a9a1
Jade K <jade@omni.co>
2025-01-17 22:07:59 +0000

    observations

--- models/01-intermediate/fhir/int_fhir__observations.sql
+++ models/01-intermediate/fhir/int_fhir__observations.sql
@@ -1,2 +1,5 @@
-select {{ dbt_utils.star(ref("stg_fhir__observations")) }}
+select 
+    timestampadd(year, extract(year, current_date()) - 2017, effective_ts) as effective_ts,
+    timestampadd(year, extract(year, current_date()) - 2017, issued_ts) as issued_ts,
+    {{ dbt_utils.star(ref("stg_fhir__observations"), except=["effective_ts", "issued_ts"]) }}
 from {{ ref("stg_fhir__observations") }}
--- models/02-mart/fhir/observations.sql
+++ models/02-mart/fhir/observations.sql
@@ -1,2 +1,2 @@
 select {{ dbt_utils.star(ref("stg_fhir__observations")) }}
-from {{ ref("stg_fhir__observations") }}
+from {{ ref("int_fhir__observations") }}
