a5fd5f9246168510adfb9f064c1cddf6da99f5ed
Jade K <jade@omni.co>
2025-01-17 22:19:23 +0000

    precedure

--- models/02-mart/fhir/procedures.sql
+++ models/02-mart/fhir/procedures.sql
@@ -1,11 +1,4 @@
 select
-    {{
-        dbt_utils.star(
-            from=ref("stg_fhir__procedures"),
-            except=["performed_ts", "performed_start_ts", "performed_end_ts"],
-        )
-    }},
-    case when performed_ts is not null then true else false end as is_quick_procedure,
-    coalesce(performed_ts, performed_start_ts) as performed_start_ts,
-    coalesce(performed_ts, performed_end_ts) as performed_end_ts
-from {{ ref("stg_fhir__procedures") }}
+  {{ dbt_utils.star(ref('stg_fhir__procedures')) }},
+  case when performed_ts is not null then true else false end as is_quick_procedure
+from {{ ref('int_fhir__procedures') }}
