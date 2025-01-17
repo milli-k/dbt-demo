c52f5360d843ebaf4e414845227b46aefdbab236
Jade K <jade@omni.co>
2025-01-17 22:23:29 +0000

    fix procedure

--- models/01-intermediate/fhir/int_fhir__procedures.sql
+++ models/01-intermediate/fhir/int_fhir__procedures.sql
@@ -5,6 +5,7 @@ select
             except=["performed_ts", "performed_start_ts", "performed_end_ts"],
         )
     }},
+    case when performed_ts is not null then true else false end as is_quick_procedure,
     timestampadd(year, extract(year, current_date()) - 2017, coalesce(performed_ts, performed_start_ts)) as performed_start_ts,
     timestampadd(year, extract(year, current_date()) - 2017, coalesce(performed_ts, performed_end_ts)) as performed_end_ts
 from {{ ref("stg_fhir__procedures") }}
--- models/02-mart/fhir/procedures.sql
+++ models/02-mart/fhir/procedures.sql
@@ -1,4 +1,3 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__procedures')) }},
-  case when performed_ts is not null then true else false end as is_quick_procedure
+  {{ dbt_utils.star(ref('int_fhir__procedures')) }}
 from {{ ref('int_fhir__procedures') }}
