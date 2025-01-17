b2230dd69b29c82e82d5bb947371eef52adee66c
Jade K <jade@omni.co>
2025-01-17 22:05:13 +0000

    medication_requests

--- models/01-intermediate/fhir/int_fhir__medication_requests.sql
+++ models/01-intermediate/fhir/int_fhir__medication_requests.sql
@@ -1,3 +1,5 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__medication_requests')) }}
+    timestampadd(year, extract(year, current_date()) - 2017, authored_on) as authored_on,
+    {{ dbt_utils.star(ref("stg_fhir__medication_requests"), except=["authored_on"]) }}
 from {{ ref('stg_fhir__medication_requests') }}
+
