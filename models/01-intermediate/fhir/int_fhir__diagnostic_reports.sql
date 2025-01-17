7085e5395fbb63c2cbb923082383545f2d6332c9
Jade K <jade@omni.co>
2025-01-17 21:45:20 +0000

    diagnostic reports

--- models/01-intermediate/fhir/int_fhir__diagnostic_reports.sql
+++ models/01-intermediate/fhir/int_fhir__diagnostic_reports.sql
@@ -1,3 +1,5 @@
 select
-  {{ dbt_utils.star(ref('stg_fhir__diagnostic_reports')) }}
+  timestampadd(year, extract(year, current_date()) - 2017, effective_ts) as effective_ts,
+  timestampadd(year, extract(year, current_date()) - 2017, issued_ts) as issued_ts,
+  {{ dbt_utils.star(ref('stg_fhir__diagnostic_reports'), except=["effective_ts", "issued_ts"]) }}
 from {{ ref('stg_fhir__diagnostic_reports') }}
