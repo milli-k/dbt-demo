b7bdaca035fbe6113712fe21cbcfd6dc4aec66db
Jade K <jade@omni.co>
2025-11-14 21:55:36 +0000

    revert change

--- models/00-staging/saas/stg_saas__account.sql
+++ models/00-staging/saas/stg_saas__account.sql
@@ -1,7 +1,2 @@
-{{ log("Current role: " ~ target.role, info=True) }}
-{{ log("Current user: " ~ target.user, info=True) }}
-{{ log("Current database: " ~ target.database, info=True) }}
-{{ log("Current warehouse: " ~ target.warehouse, info=True) }}
-
-select *
+select {{ dbt_utils.star(source('saas', 'account')) }}
 from {{ source('saas', 'account') }}
