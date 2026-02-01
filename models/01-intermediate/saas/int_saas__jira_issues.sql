d7a0dca70841b4a5a0d80a334c299f307b5e1547
Peter Whitehead <peter@omni.co>
2026-02-01 17:30:35 +0000

    Updated the models for saas to make the data "live"

--- models/01-intermediate/saas/int_saas__jira_issues.sql
+++ models/01-intermediate/saas/int_saas__jira_issues.sql
@@ -1,2 +1,30 @@
-select * from {{ ref('stg_saas__jira_issues') }}
-WHERE created_date < current_date
+WITH params AS (
+  SELECT 
+    CURRENT_DATE() AS as_of_date,
+    (YEAR(CURRENT_DATE()) - 2025) AS year_offset
+)
+
+SELECT 
+    * REPLACE (
+        -- 1. Shift Created Date (Original Logic)
+        DATEADD(month, p.year_offset * 12, "created_date") AS "created_date",
+
+        -- 2. Logic: If shifted Updated Date is in the future, cap it at TODAY
+        IFF(
+            DATEADD(month, p.year_offset * 12, "updated_date") > p.as_of_date,
+            p.as_of_date,
+            DATEADD(month, p.year_offset * 12, "updated_date")
+        ) AS "updated_date",
+
+        -- 3. Logic: If shifted Resolved Date is in the future, it's not resolved yet (NULL)
+        IFF(
+            DATEADD(month, p.year_offset * 12, "resolved_date") > p.as_of_date,
+            NULL,
+            DATEADD(month, p.year_offset * 12, "resolved_date")
+        ) AS "resolved_date"
+    )
+FROM {{ ref('stg_saas__jira_issues') }}
+CROSS JOIN params p
+-- Only show issues that have been 'created' in our 2026 timeline
+WHERE DATEADD(month, p.year_offset * 12, "created_date") <= p.as_of_date
+ORDER BY "created_date" DESC
--- models/01-intermediate/saas/int_saas__sessions.sql
+++ models/01-intermediate/saas/int_saas__sessions.sql
@@ -1,2 +1,51 @@
-select * from {{ ref('stg_saas__sessions') }}
-WHERE session_end_time < current_timestamp
+WITH params AS (
+  SELECT 
+    CURRENT_DATE() AS as_of_date,
+    (YEAR(CURRENT_DATE()) - 2025) AS year_offset
+)
+
+SELECT
+    "id",
+    "account_id",
+    "user_id",
+    "session_duration_seconds",
+    "session_duration_minutes",
+    "product_id",
+    "pages_visited",
+    "events_count",
+    "unique_pages",
+    "bounce_rate",
+    "exit_page",
+    "entry_page",
+    "device_type",
+    "browser",
+    "os",
+    "country",
+    "region",
+    "city",
+    "ip_address",
+    "user_agent",
+    "referrer",
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "account_segment",
+    "product_tier",
+    "user_count",
+    "user_role",
+    "session_quality_score",
+    "errors_encountered",
+    "conversion_events",
+    
+    -- Shift the Date field
+    DATEADD(month, p.year_offset * 12, "session_date") AS "session_date",
+    
+    -- Shift the Timestamps
+    DATEADD(month, p.year_offset * 12, "session_start_time") AS "session_start_time",
+    DATEADD(month, p.year_offset * 12, "session_end_time") AS "session_end_time"
+
+FROM {{ ref('stg_saas__sessions') }}
+CROSS JOIN params p
+-- Filter so sessions that haven't "happened yet" in our 2026 timeline are hidden
+WHERE DATEADD(month, p.year_offset * 12, "session_start_time") < CURRENT_TIMESTAMP()
+ORDER BY "session_date" DESC
